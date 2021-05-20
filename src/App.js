cf0473eb19d688a11bfe0f314ca99eff92372e6b
unknown <Olorunda Samuel>
2021-05-20 16:02:22 +0100

    Test Pratice

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import ReactShareSocial from "./Components/SocialShareTest/ReactShareSocially.js";
+import SocialTest from "./Components/SocialShareTest/SocialTest";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <SocialTest />
+        {/* <ReactShareSocial /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
